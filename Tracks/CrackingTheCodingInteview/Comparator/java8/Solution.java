2ddbc4d9624ad006ca52987d1ed7c2435835618d
samhal <halali@student.chalmers.se>
2017-12-03 14:20:21 +0100

    Minor fix

--- Tracks/CrackingTheCodingInteview/Comparator/java8/Solution.java
+++ Tracks/CrackingTheCodingInteview/Comparator/java8/Solution.java
@@ -1,6 +1,6 @@
 class Checker implements Comparator<Player> {
 	public int compare(Player a, Player b) {
 		int scoreComparison = Integer.compare(b.score, a.score);
-		return (scoreComparison == 0) ? b.name.compareTo(a.name) : scoreComparison;
+		return (scoreComparison == 0) ? a.name.compareTo(b.name) : scoreComparison;
 	}
 }
