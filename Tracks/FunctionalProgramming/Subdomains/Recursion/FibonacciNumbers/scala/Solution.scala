09421dbcbeaab60855f011dc7ab5c2310ef1d301
samhal <halali@student.chalmers.se>
2018-03-16 20:58:06 +0100

    Add scala solution for Fibonacci numbers

---
+++ Tracks/FunctionalProgramming/Subdomains/Recursion/FibonacciNumbers/scala/Solution.scala
@@ -0,0 +1,12 @@
+object Solution {
+    
+  def fibonacci(x:Int):Int = {  
+    if (x <= 2) x - 1
+    else fibonacci(x - 1) + fibonacci(x - 2)
+  }
+
+  def main(args: Array[String]) {
+    /** This will handle the input and output**/
+    println(fibonacci(readInt()))
+  }
+}
